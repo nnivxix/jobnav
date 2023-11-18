7ec76693efff9b4ad99a5545ff9bb509f90ee024
nnivxix <adhe.hanasa@gmail.com>
2023-11-18 22:52:30 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
+  hooks: {
+    "components:dirs": (dirs) => {
+      dirs.unshift({
+        path: "~/components/ui",
+        // this is required else Nuxt will autoImport `.ts` file
+        extensions: [".vue"],
+        // prefix for your components, eg: UiButton
+        prefix: "",
+        // prevent adding another prefix component by it's path.
+        pathPrefix: false,
+      });
+    },
+  },
+});
