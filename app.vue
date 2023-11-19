716ea1799d04dbb1056487147d09d71d1b89be8a
nnivxix <adhe.hanasa@gmail.com>
2023-11-19 12:07:42 +0700

    feat: desktop navbar

--- app.vue
+++ app.vue
@@ -1,45 +1,5 @@
-<script setup lang="ts">
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
-
-const defaultValue = "item-1";
-
-const accordionItems = [
-  {
-    value: "item-1",
-    title: "Is it accessible?",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.",
-  },
-  {
-    value: "item-2",
-    title: "Is it unstyled?",
-    content: "Yes. It's unstyled by default, giving you freedom over the look and feel.",
-  },
-  {
-    value: "item-3",
-    title: "Can it be animated?",
-    content: "Yes! You can use the transition prop to configure the animation.",
-  },
-];
-</script>
-
 <template>
-  <div>
-    <Accordion type="single" class="w-full" collapsible :default-value="defaultValue">
-      <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
-        <AccordionTrigger>{{ item.title }}</AccordionTrigger>
-        <AccordionContent>
-          {{ item.content }}
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  </div>
-  <div>
-    <Button variant="ghost" class="p-2" @click="toggleDark()">
-      <Icon name="ph:moon-fill" v-if="!isDark" class="h-5 w-5" />
-      <Icon name="ph:sun-dim-fill" v-else class="h-5 w-5" />
-      <span class="sr-only">Toggle dark mode</span>
-    </Button>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+const centerLinks = ref([
+  {
+    name: "Find Jobs",
+    destination: "/jobs",
+  },
+  {
+    name: "Companies",
+    destination: "/companies",
+  },
+  {
+    name: "Tips",
+    destination: "/tips",
+  },
+  {
+    name: "Services",
+    destination: "/services",
+  },
+]);
+
+const rightLinks = ref([
+  {
+    name: "Login",
+    destination: "/login",
+  },
+  {
+    name: "Sign up",
+    destination: "/sign-up",
+  },
+]);
+</script>
+<template>
+  <nav class="hidden lg:grid grid-cols-12 gap-4 max-w-7xl mx-auto text-lg font-semibold mt-4">
+    <NuxtLink to="/" class="col-span-3 py-2 font-bold"> Jobnav </NuxtLink>
+    <div class="col-span-6 place-self-center">
+      <NuxtLink
+        v-for="(centerLink, index) in centerLinks"
+        :key="index"
+        :to="centerLink.destination"
+        class="px-5 py-2"
+      >
+        {{ centerLink.name }}
+      </NuxtLink>
+    </div>
+    <!-- Condition if unauthenticated -->
+    <div class="col-span-3 place-self-end self-center">
+      <NuxtLink
+        v-for="(rightLink, index) in rightLinks"
+        :key="index"
+        :to="rightLink.destination"
+        class="first:pr-5 py-2 [&:nth-child(2)]:py-2 px-5 [&:nth-child(2)]:bg-[#FFB84C] [&:nth-child(2)]:rounded-lg [&:nth-child(2)]:text-white"
+      >
+        {{ rightLink.name }}
+      </NuxtLink>
+    </div>
+    <!-- Condition if authenticated -->
+    <!-- Start code here -->
+    <!-- End code here -->
+  </nav>
+  <nav></nav>
+</template>
+
+<style scoped></style>
