99955c4118ac33b275be37a0da97a8275b5ee815
nnivxix <adhe.hanasa@gmail.com>
2023-11-25 20:35:35 +0700

    feat: save to localstorage after using api login

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,3 +1,5 @@
+import { useStorage, StorageSerializers } from "@vueuse/core";
+
 export type LoginCredentials = {
   email: string;
   password: string;
@@ -5,11 +7,18 @@ export type LoginCredentials = {
 export const useAuth = () => {
   async function login(credentials: LoginCredentials) {
     useLaravelFetch("/sanctum/csrf-cookie");
-    const { data } = useLaravelFetch("/api/login", {
+    const { data } = await useLaravelFetch("/api/login", {
       method: "POST",
       body: credentials,
     });
-    console.log(data);
+    const user = useStorage("user", {}, undefined, { serializer: StorageSerializers.object });
+    user.value = {
+      token: data.value?.token,
+      id: data.value.data.id,
+      email: data.value.data.email,
+      name: data.value.data.name,
+      username: data.value.data.username,
+    };
   }
 
   return {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon", "@vee-validate/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@vee-validate/nuxt",
+    "@pinia/nuxt",
+  ],
   runtimeConfig: {
     public: {
       backendUrl: "http://localhost:8000",
